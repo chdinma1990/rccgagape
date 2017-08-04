811c8879f086a706ece043ccac4635f856e06d0c
Chdinma1990 <nonsowinner@gmail.com>
2017-08-04 07:57:18 -0500

    add recent changes to app

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,10 @@
 class WelcomeController < ApplicationController
+  
+  def contact_us
+  end
+
+
   def index
   end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
-
-  root 'welcome#index'
+  get 'contact_us'=> "home#contact_us"
+  root 'home#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
